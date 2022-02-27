9d4612a23a55390a3676ec21a45f013dc9f18c73
mufcberry <reagan.berry2@gmail.com>
2022-02-27 13:29:39 +0000

    Created Timer Text

--- Assets/CountdownTimer.cs
+++ Assets/CountdownTimer.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CountdownTimer : MonoBehaviour
 {
-    float currentTime = 0f;
-    float startingTime = 120f;
-
+    public float currentTime = 0f;
+    public float startingTime = 120f;
+    private TextMeshProUGUI textMesh;
+    
     // Start is called before the first frame update
     void Start()
     {
+        textMesh = gameObject.GetComponent<TextMeshProUGUI>();
         currentTime = startingTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        textMesh.text = currentTime.ToString();
         currentTime -=1 * Time.deltaTime;
     }
 }
